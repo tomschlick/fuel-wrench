d06e1e6f674ea3eedf7faa2c44feabd33964b964
Tom Schlick <tom@tomschlick.com>
2012-03-14 01:22:40 -0400

    now returning the response as per fuel's new style

--- classes/httpserviceunavailableexception.php
+++ classes/httpserviceunavailableexception.php
@@ -2,14 +2,8 @@
 
 class HttpServiceUnavailableException extends Fuel\Core\HttpException
 {
-	/**
-	 * When this type of exception isn't caught this method is called by
-	 * Error::exception_handler() to deal with the problem.
-	 */
-	public function handle()
+	public function response()
 	{
-		$response = new \Response(\View::forge('503'), 503);
-		\Event::shutdown();
-		$response->send(true);
+		return new \Response(\View::forge('503'), 503);
 	}
 }
