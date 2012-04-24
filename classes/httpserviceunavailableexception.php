c01080094d002d6f846176b6f8e4c15af9f072af
Tom Schlick <tom@tomschlick.com>
2012-04-24 00:31:57 -0400

    moving the 503.php inside of errors

--- classes/httpserviceunavailableexception.php
+++ classes/httpserviceunavailableexception.php
@@ -4,6 +4,6 @@ class HttpServiceUnavailableException extends Fuel\Core\HttpException
 {
 	public function response()
 	{
-		return new \Response(\View::forge('503'), 503);
+		return new \Response(\View::forge('error/503'), 503);
 	}
 }
