b891647d15a364e0e5941542599b701a64fac20b
Tom Schlick <tom@tomschlick.com>
2012-09-30 22:45:00 -0400

    now logging requests that hit the HttpServiceUnavailableException that wrench throws

--- classes/httpserviceunavailableexception.php
+++ classes/httpserviceunavailableexception.php
@@ -4,6 +4,7 @@ class HttpServiceUnavailableException extends Fuel\Core\HttpException
 {
 	public function response()
 	{
+		\Log::debug('HttpServiceUnavailableException thrown because Wrench is active ('.Uri::current().')');
 		return new \Response(\View::forge('error/503'), 503);
 	}
 }
